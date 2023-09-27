c2a176bc437edcdb1fd132807379138fcba426d1
emil_ <emil.kushner.stud@mail.ru>
2023-09-27 12:57:15 +0700

    Попытка исправления ошибки

--- app/src/main/java/com/example/figna/MainActivity.kt
+++ app/src/main/java/com/example/figna/MainActivity.kt
@@ -14,9 +14,9 @@ class MainActivity : AppCompatActivity() {
         val input_text = findViewById<EditText>(R.id.edit1)
         val output_text = findViewById<TextView>(R.id.text2)
         when(input_text.text){
-            "L", "M", "K", "D" -> "Это согласные буквы"
-            "A".."Z" -> "Возможно, это гласные буквы"
-            else -> "Введён не символ,"
+            "L", "M", "K", "D" -> input_text.text = "Это согласные буквы"
+            "A".."Z" -> input_text.text = "Возможно, это гласные буквы"
+            else -> input_text.text = "Введён не символ"
         }
     }
 }

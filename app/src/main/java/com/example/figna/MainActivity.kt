2f2f35400a37429c19eadda8f634e5fba69cfc61
emil_ <emil.kushner.stud@mail.ru>
2023-09-27 12:51:27 +0700

    Добавлена реакция кнопки

--- app/src/main/java/com/example/figna/MainActivity.kt
+++ app/src/main/java/com/example/figna/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.figna
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val input_text = findViewById<EditText>(R.id.edit1)
+        val output_text = findViewById<TextView>(R.id.text2)
+        when(input_text.text){
+            "L", "M", "K", "D" -> "Это согласные буквы"
+            "A".."Z" -> "Возможно, это гласные буквы"
+            else -> "Введён не символ,"
+        }
     }
 }
